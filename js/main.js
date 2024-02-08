9331bc011ca90e4a62c995a98f89d3455d9e2546
kyung-doo <hohomkd@naver.com>
2024-02-08 10:58:15 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@
       'video4.mp4',
       'video5.mp4',
       'video6.mp4',
-      'video7.mp4',
-      'video8.mp4',
-      'video9.mp4',
-      'video10.mp4',
+      'video1.mp4',
+      'video2.mp4',
+      'video3.mp4',
+      'video4.mp4',
    ];
 
    let count = 0;
